bb616b60d7d97e71dfbbef366ecd29ec9e3b4ab6
creesmithers <cree.smithers@gmail.com>
2026-06-25 09:40:48 -0700

    Updated CSV added Temp.py
    updated CSV to have all headers.
    
    Including temp.py for bugfixing and finding errors in code

---
+++ temp.py
@@ -0,0 +1,15 @@
+import pandas as pd
+# df = pd.read_csv('mlb_standings.csv')
+
+
+# with open('mlb_standings.csv', 'r') as f:
+#     lines = f.readlines()
+
+# print(f"Total lines: {len(lines)}")
+# print(f"Last 5 lines:")
+# for line in lines[-5:]:
+#     print(line.strip())
+
+
+import os
+print(os.path.abspath('mlb_standings.csv'))
